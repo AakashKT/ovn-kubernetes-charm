7fc04a5ba713f51c7c6ec5dde3f71927b93074af
AakashKT <aakash.kt@research.iiit.ac.in>
2017-07-01 23:07:31 +0530

    Added bridge to gateway node to handle both mgmt and ssh traffic

--- layers/k8s-gateway/reactive/k8s_gateway.py
+++ layers/k8s-gateway/reactive/k8s_gateway.py
@@ -64,17 +64,30 @@ def install_k8s_gateway(k8s_master):
     os.chdir('/tmp/ovn-kubernetes');
     run_command('sudo pip2 install .');
 
+    op = run_command('ovn-k8s-util nics-to-bridge ens4');
+    log('Bridge create output :');
+    log(op);
+
+    op = run_command('dhclient -r brens4');
+    log('Release interface : %s' % (op));
+    op = run_command('dhclient brens4');
+    log('Fresh assign : %s' % (op));
+
     local_ip = get_my_ip();
     gateway_ip = run_command('ip route | grep default').split(' ')[2];
 
     op = run_command('ovn-k8s-overlay gateway-init \
                     --cluster-ip-subnet="192.168.0.0/16" \
-                    --physical-interface ens4 \
+                    --bridge-interface brens4 \
                     --physical-ip %s/24 \
                     --node-name="kube-minion2" \
                     --default-gw %s' % (local_ip, gateway_ip));
     log('Gateway init output :');
     log(op);
+
+    op = run_command('ovn-k8s-gateway-helper --physical-bridge=brens4 \
+                        --physical-interface=ens4 --pidfile --detach');
+    log('Daemon start : %s' % (op));
 
     set_state('k8s-gateway.installed')
 
