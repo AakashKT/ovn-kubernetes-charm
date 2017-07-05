c90d714f55824cf2488cecb8063e3709f978c6fc
AakashKT <aakash.kt@research.iiit.ac.in>
2017-07-05 23:03:24 +0530

    Added bundle file Kubernetes workingy

--- layers/k8s-gateway/reactive/k8s_gateway.py
+++ layers/k8s-gateway/reactive/k8s_gateway.py
@@ -53,12 +53,8 @@ def get_my_ip():
 # Hooks and reactive handlers
 #########################################################################
 
-@when('k8s-master-config.available')
 @when_not('k8s-gateway.installed')
-def install_k8s_gateway(k8s_master):
-    k8s_api_ip = k8s_master.get_config('k8s_api_ip');
-    run_command('ovs-vsctl set Open_vSwitch . \
-                    external_ids:k8s-api-server="%s:8080"' % (k8s_api_ip));
+def install_k8s_gateway():
 
     run_command('git clone https://github.com/openvswitch/ovn-kubernetes /tmp/ovn-kubernetes');
     os.chdir('/tmp/ovn-kubernetes');
@@ -75,13 +71,14 @@ def install_k8s_gateway(k8s_master):
 
     local_ip = get_my_ip();
     gateway_ip = run_command('ip route | grep default').split(' ')[2];
+    hostname = run_command('hostname');
 
     op = run_command('ovn-k8s-overlay gateway-init \
                     --cluster-ip-subnet="192.168.0.0/16" \
                     --bridge-interface brens4 \
                     --physical-ip %s/24 \
-                    --node-name="kube-minion2" \
-                    --default-gw %s' % (local_ip, gateway_ip));
+                    --node-name="%s-gateway" \
+                    --default-gw %s' % (local_ip, hostname, gateway_ip));
     log('Gateway init output :');
     log(op);
 
@@ -90,20 +87,3 @@ def install_k8s_gateway(k8s_master):
     log('Daemon start : %s' % (op));
 
     set_state('k8s-gateway.installed')
-
-
-@when('ovn-central-config.available')
-@when_not('onetime-setup.done')
-def onetime_setup(ovn_central):
-    central_ip = ovn_central.get_config('central_ip');
-    local_ip = get_my_ip();
-
-    run_command('ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:%s:6642" \
-                        external_ids:ovn-nb="tcp:%s:6641" \
-                        external_ids:ovn-encap-ip=%s \
-                        external_ids:ovn-encap-type="%s"' % (central_ip, central_ip, local_ip, 'stt'));
-
-    run_command('ovs-vsctl set Open_vSwitch . external_ids:system-id=$(uuidgen)');
-    run_command('/usr/share/openvswitch/scripts/ovn-ctl start_controller');
-
-    set_state('onetime-setup.done');
--- layers/k8s-master/reactive/k8s_master.py
+++ layers/k8s-master/reactive/k8s_master.py
@@ -53,45 +53,59 @@ def get_my_ip():
 # Hooks and reactive handlers
 #########################################################################
 
-@when('onetime-setup.done')
-@when_not('k8s-master.installed')
-def install_k8s_master():
-
-    run_command('ovs-vsctl set Open_vSwitch . external_ids:k8s-api-server="127.0.0.1:8080"');
-
-    run_command('git clone https://github.com/openvswitch/ovn-kubernetes /tmp/ovn-kubernetes');
-
-    os.chdir('/tmp/ovn-kubernetes');
-
-    run_command('sudo pip2 install .');
-    run_command('ovn-k8s-overlay master-init --cluster-ip-subnet="192.168.0.0/16" \
-                    --master-switch-subnet="192.168.1.0/24" --node-name="kube-master" ')
-
-    set_state('k8s-master.installed')
-
-
-@when('ovn-central-config.available')
-@when_not('onetime-setup.done')
-def onetime_setup(ovn_central):
-    central_ip = ovn_central.get_config('central_ip');
-    local_ip = get_my_ip();
-
-    run_command('ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:%s:6642" \
-                        external_ids:ovn-nb="tcp:%s:6641" \
-                        external_ids:ovn-encap-ip=%s \
-                        external_ids:ovn-encap-type="%s"' % (central_ip, central_ip, local_ip, 'stt'));
-
-    run_command('ovs-vsctl set Open_vSwitch . external_ids:system-id=$(uuidgen)');
-    run_command('/usr/share/openvswitch/scripts/ovn-ctl start_controller');
-
-    set_state('onetime-setup.done');
-
-
-@when('k8s-master-config.available', 'k8s-master.installed')
-def broadcast_config(k8s_master):
-    k8s_api_ip = get_my_ip();
+@when('central-config.available')
+def broadcast_config(ovn_central):
+    central_ip = get_my_ip();
 
     config = {
-        'k8s_api_ip' : k8s_api_ip,
+        'central_ip' : central_ip,
     };
-    k8s_master.send_config(config);
+    ovn_central.send_config(config);
+
+@when_not('ovn-central.installed')
+def install_k8s_master():
+    central_ip = get_my_ip();
+    hostname = run_command('hostname');
+
+    run_command('/usr/share/openvswitch/scripts/ovn-ctl start_northd');
+
+    """
+    os.chdir('/etc/openvswitch');
+    run_command('sudo ovs-pki -d /vagrant/pki init --force');
+    run_command('sudo ovs-pki req ovnsb && sudo ovs-pki self-sign ovnsb');
+    run_command('sudo ovn-sbctl set-ssl /etc/openvswitch/ovnsb-privkey.pem \
+                    /etc/openvswitch/ovnsb-cert.pem  /vagrant/pki/switchca/cacert.pem');
+    run_command('sudo ovs-pki req ovnnb && sudo ovs-pki self-sign ovnnb');
+    run_command('sudo ovn-nbctl set-ssl /etc/openvswitch/ovnnb-privkey.pem \
+                    /etc/openvswitch/ovnnb-cert.pem  /vagrant/pki/switchca/cacert.pem');
+    run_command('sudo ovs-pki req ovncontroller');
+    run_command('sudo ovs-pki -b -d /vagrant/pki sign ovncontroller switch');
+    ovn_host_file = open('/etc/default/ovn-host', 'a');
+    ovn_host_file.write('OVN_CTL_OPTS="--ovn-controller-ssl-key=/etc/openvswitch/ovncontroller-privkey.pem  \
+                    --ovn-controller-ssl-cert=/etc/openvswitch/ovncontroller-cert.pem \
+                    --ovn-controller-ssl-bootstrap-ca-cert=/etc/openvswitch/ovnsb-ca.cert"');
+    ovn_host_file.close();
+    """
+
+    run_command('ovn-nbctl set-connection ptcp:6641');
+    run_command('ovn-sbctl set-connection ptcp:6642');
+
+    run_command('ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:%s:6642" \
+                    external_ids:ovn-nb="tcp:%s:6641" \
+                    external_ids:ovn-encap-ip=%s \
+                    external_ids:ovn-encap-type="%s"' % (central_ip, central_ip, central_ip, 'geneve'));
+
+    run_command('/usr/share/openvswitch/scripts/ovn-ctl restart_controller');
+    run_command('sudo ovs-vsctl set Open_vSwitch . external_ids:k8s-api-server="0.0.0.0:8080"');
+
+    run_command('git clone https://github.com/openvswitch/ovn-kubernetes /tmp/ovn-kubernetes');
+    os.chdir('/tmp/ovn-kubernetes');
+    run_command('sudo -H pip2 install .');
+    run_command('sudo ovn-k8s-overlay master-init --cluster-ip-subnet="192.168.0.0/16" \
+                    --master-switch-subnet="192.168.1.0/24" \
+                    --node-name="%s"' % (hostname));
+
+    run_command('sudo ovn-k8s-watcher --overlay --pidfile --log-file -vfile:info \
+                    -vconsole:emer --detach');
+
+    set_state('ovn-central.installed');
